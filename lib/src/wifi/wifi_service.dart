ab4e019bef3fd2b9f8f69106dab16056dcd59b22
DKKim <dgkim@obelab.com>
2026-04-24 16:02:10 +0900

    [fix] check platform is windows and call function for windows

--- lib/src/wifi/wifi_service.dart
+++ lib/src/wifi/wifi_service.dart
@@ -1,8 +1,13 @@
 
+import 'dart:io';
+
+import 'package:nirsit_plugin/src/wifi/wifi_windows.dart';
 import 'package:wifi_iot/wifi_iot.dart';
 import 'package:nirsit_plugin/src/utils/logger.dart';
 import 'package:wifi_scan/wifi_scan.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+
+import '../data/wifi/wifi_network_info.dart';
 export 'package:wifi_scan/wifi_scan.dart' show WiFiAccessPoint;
 
 class WifiService {
@@ -10,21 +15,29 @@ class WifiService {
   final connectivity = Connectivity();
   Stream<List<ConnectivityResult>> get connectivityResultStream => connectivity.onConnectivityChanged;
 
-  Future<List<WiFiAccessPoint>> scan() async {
-    final wifiScan = WiFiScan.instance;
-    final canScan = await wifiScan.canStartScan(askPermissions: true);
-
-    if (canScan != CanStartScan.yes) {
-      throw WifiScanException(canScan.toString());
-    }
-    await wifiScan.startScan();
-
+  Future<List<WifiNetworkInfo>> scan() async {
     try {
-      final result = await wifiScan.getScannedResults();
-      return Future.value(result.where((element) {
-        logger.d('plug-in :  ssid: ${element.ssid}');
-        return element.ssid.startsWith('NIRSIT');
-      }).toList());
+      if (Platform.isWindows) {
+        final result = await WifiWindows.scan();
+        return Future.value(result.where((element) {
+          logger.d('plug-in :  ssid: ${element.ssid}');
+          return element.ssid.startsWith('NIRSIT');
+        }).toList());
+      } else {
+        final wifiScan = WiFiScan.instance;
+        final canScan = await wifiScan.canStartScan(askPermissions: true);
+
+        if (canScan != CanStartScan.yes) {
+          throw WifiScanException(canScan.toString());
+        }
+        await wifiScan.startScan();
+
+        final result = await wifiScan.getScannedResults();
+        return Future.value(result.where((element) {
+          logger.d('plug-in :  ssid: ${element.ssid}');
+          return element.ssid.startsWith('NIRSIT');
+        }).map((element) => WifiNetworkInfo(element.ssid, element.bssid, element.level)).toList());
+      }
     } catch (e) {
       throw WifiScanException("Wifi Scan Error $e");
     }
@@ -34,29 +47,30 @@ class WifiService {
     return await WiFiForIoTPlugin.getSSID();
   }
 
-  Future<bool> isConnected() async {
-    return await WiFiForIoTPlugin.isConnected();
-  }
+  Future<bool> isConnected() => Platform.isWindows ? WifiWindows.isConnected() : WiFiForIoTPlugin.isConnected();
 
-  Future<bool> disconnect() async {
-    return await WiFiForIoTPlugin.disconnect();
-  }
+  Future<bool> disconnect() => Platform.isWindows ? WifiWindows.disconnect() : WiFiForIoTPlugin.disconnect();
 
-  Future<bool> isWifiEnabled() async {
-    return await WiFiForIoTPlugin.isEnabled();
-  }
+  Future<bool> isWifiEnabled() => Platform.isWindows ? WifiWindows.isEnabled() : WiFiForIoTPlugin.isEnabled();
 
   Future<bool> registerWifiNetwork(String ssid, String bssid, String password) async {
+    if (Platform.isWindows) return false;
     return await WiFiForIoTPlugin.registerWifiNetwork(ssid, bssid: bssid, password: password,security: NetworkSecurity.WPA);
   }
 
   Future<bool> connect(String ssid, String bssid, String password) async {
-    logger.d('plug-in :  ssid: $ssid, password: $password');
-    bool connected = await WiFiForIoTPlugin
-        .connect(ssid, bssid: bssid, password: password, joinOnce: false, security: NetworkSecurity.WPA, withInternet: false);
-    bool forceWifiUsage = await WiFiForIoTPlugin.forceWifiUsage(true);
-    logger.d('plug-in :  connected: $connected, forceWifiUsage: $forceWifiUsage');
-    return connected;
+    if (Platform.isWindows) {
+      final connected = await WifiWindows.connect(ssid, password);
+      logger.d('plug-in : windows connected: $connected');
+      return connected;
+    } else {
+      logger.d('plug-in :  ssid: $ssid, password: $password');
+      bool connected = await WiFiForIoTPlugin
+          .connect(ssid, bssid: bssid, password: password, joinOnce: false, security: NetworkSecurity.WPA, withInternet: false);
+      bool forceWifiUsage = await WiFiForIoTPlugin.forceWifiUsage(true);
+      logger.d('plug-in :  connected: $connected, forceWifiUsage: $forceWifiUsage');
+      return connected;
+    }
   }
 }
 
